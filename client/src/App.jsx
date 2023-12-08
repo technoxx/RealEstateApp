e1ed2f030bf462112e38d5f3a070cceba4445a56
technoxx <somiyaagarwal2004@gmail.com>
2023-12-08 20:40:19 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/sign-in" element={<Signin />}></Route>
+          <Route path="/sign-up" element={<Signup />}></Route>
+          <Route path='/search' element={<Search />} />
+
+
+          <Route path="/listing/:listingId" element={<Listing />}></Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/create-listing" element={<CreateListing />}></Route>
+            <Route path="/update-listing/:listingId" element={<UpdateListing />}></Route>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCj63b5KQmxWwzuXrLtu37ravwZZ1vp4sY",
+  authDomain: "realestateapp-e630f.firebaseapp.com",
+  projectId: "realestateapp-e630f",
+  storageBucket: "realestateapp-e630f.appspot.com",
+  messagingSenderId: "137056285663",
+  appId: "1:137056285663:web:77baea6d27545292078a6a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
